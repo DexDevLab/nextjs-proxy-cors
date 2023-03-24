f50c0aa7440ca7a7a7042f908e6b8a528278c621
Daniel Augusto <dex.houshi@hotmail.com>
2023-03-24 15:43:24 -0300

    v0.0.7-230324
    - Adição de dependências e testes de execução no Vercel

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,20 +1,12 @@
-// Pinboard API proxy.
-// For whatever reason the Pinboard API (https://pinboard.in/howto/#api) doesn't
-// have CORS enabled — so you can't make API calls directly from the browser.
-// This small server proxies requests to the Pinboard API allowing CORS.
-
-// Create a proxy to redirect requests of the "/api/*" path to the Pinboard API.
-// Examples:
-// GET /api/v1/posts/all?format=json → https://api.pinboard.in/v1/posts/all?format=json
 import { createProxyMiddleware } from "http-proxy-middleware";
 import NextCors from "nextjs-cors";
 
-const apiProxy = createProxyMiddleware(url, {
-  target: url,
+const apiProxy = createProxyMiddleware({
   changeOrigin: true,
   pathRewrite: {
     "^/api": "",
   },
+  router: (req) => customRouter(req),
   onProxyRes(proxyRes) {
     (proxyRes.headers["access-control-allow-origin"] = "*"),
       (proxyRes.headers["access-control-allow-methods"] =
@@ -24,11 +16,23 @@ const apiProxy = createProxyMiddleware(url, {
   },
 });
 
+const customRouter = function (req) {
+  const url = req.query["url"];
+  return url;
+};
+
 export default async function (req, res) {
   await NextCors(req, res, {
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     origin: "*",
     optionsSuccessStatus: 200,
   });
-  return apiProxy(req.query.url, req, res);
+  apiProxy(req, res);
 }
+
+export const config = {
+  api: {
+    externalResolver: true,
+    bodyParser: false,
+  },
+};
